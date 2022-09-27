0faebbc8fe3cbb3cde9755f550b829563a9bfc8b
Ibrahim <aitkazindev@gmail.com>
2022-09-27 16:51:42 +0900

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Ibrahim_ios on 2022/09/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
