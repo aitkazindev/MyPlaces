8c709184bd8f6c02eb35c696aff3041e1b97415c
Ibrahim <aitkazindev@gmail.com>
2022-09-30 09:28:20 +0900

    Action sheets modified

--- MyPlaces/NewPlaceTableViewController.swift
+++ MyPlaces/NewPlaceTableViewController.swift
@@ -22,15 +22,21 @@ class NewPlaceTableViewController: UITableViewController, UINavigationController
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         if indexPath.row == 0 {
+            
+            let cameraIcon = UIImage(imageLiteralResourceName: "camera")
+            let photoIcon = UIImage(imageLiteralResourceName: "photo")
             let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
             let camera = UIAlertAction(title: "Camera", style: .default){ _ in
                 self.chooseImagePicker(source: .camera)
             }
             
+            camera.setValue(cameraIcon, forKey: "image")
+            camera.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
             let photo = UIAlertAction(title: "Photo", style: .default) {_ in
                 self.chooseImagePicker(source: .photoLibrary)
             }
-            
+            photo.setValue(photoIcon, forKey: "image")
+            photo.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
             let cancel = UIAlertAction(title: "Cancel", style: .cancel)
             
             actionSheet.addAction(camera)
